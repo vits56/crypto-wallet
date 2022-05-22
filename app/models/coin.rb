14dcfa5a7230e103ce55b98310a23c43d9187f91
bruno bastos duarte <bastosduartebruno@gmail.com>
2022-05-22 15:54:32 -0300

    belongs to mining type association in model coin

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Coin < ApplicationRecord
+  belongs_to :mining_type
 end
--- db/migrate/20220522182653_add_mining_type_to_coins.rb
+++ db/migrate/20220522182653_add_mining_type_to_coins.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddMiningTypeToCoins < ActiveRecord::Migration[5.2]
   def change
     add_reference :coins, :mining_type, foreign_key: true
